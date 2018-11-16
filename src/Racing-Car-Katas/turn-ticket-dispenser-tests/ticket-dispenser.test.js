589b8f423ccdbd57bc2935907381841254aa641a
Michael Stötzer <m.stoetzer@meteocontrol.de>
2018-11-16 13:38:47 +0100

    add test description for ticket dispenser

--- src/Racing-Car-Katas/turn-ticket-dispenser-tests/ticket-dispenser.test.js
+++ src/Racing-Car-Katas/turn-ticket-dispenser-tests/ticket-dispenser.test.js
@@ -1,3 +1,10 @@
+// TicketDispenser exercise:
+// write the unit tests for the TicketDispenser.
+// The TicketDispenser class is designed to be used to manage
+// a queuing system in a shop. There may be more than one
+// ticket dispenser but the same ticket should not be issued
+// to two different customers.
+
 describe('Turn Ticket Dispenser', function () {
 
 	describe('TurnTicketDispenser', function () {
